5d99ec5fa2b911455ba84a3d6269b8231c5f067f
CryptoDevv <cryptoinbox@gmail.com>
2017-08-19 01:33:40 +0530

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Cryptoniumcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CryptoniumcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
